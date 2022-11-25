724ae4c4cece35ae271d589cb1500cb05fa9ee7f
Victor Chen <victorc.1@outlook.com>
2022-11-25 01:46:00 -0500

    delete util, api

--- src/util/non-null-args.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Creates an array of the non-null arguments.
- * @param {(T | null)[]} args the arguments to get non-null entries from
- * @returns {T[]} an array of the non-null arguments
- */
-export function nonNullArgs<T>(...args: (T | null)[]): T[] {
-    return args.filter(arg => arg != null);
-}
