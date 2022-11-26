0bd49725d79b2b68fbd368b0765cb49586320d0e
Victor Chen <victorc.1@outlook.com>
2022-11-26 05:12:49 -0500

    fix db init sql statements

--- src/db/db-init.ts
+++ src/db/db-init.ts
@@ -4,32 +4,33 @@ import { Database } from "sqlite3";
  * Initializes a new database.
  * @param db database object
  */
-export async function initDatabase(db: Database): Promise<void> {
-    return new Promise((resolve, reject) => {
-        db.run(
-            `CREATE TABLE Teams (
-                number int PRIMARY KEY AUTO_INCREMENT,
-                name varchar(80) NOT NULL
-            );
-            CREATE TABLE Participants (
-                id int PRIMARY KEY AUTO_INCREMENT,
-                email varchar(255) NOT NULL UNIQUE,
-                discordTag varchar(39) NOT NULL UNIQUE,
-                discordID int,
-                team REFERENCES Teams(number)
-            );
-            CREATE TABLE Judges (
-                id int PRIMARY KEY AUTO_INCREMENT,
-                email varchar(255) NOT NULL UNIQUE,
-                discordTag varchar(39) NOT NULL UNIQUE,
-                discordID int
-            );
-            CREATE TABLE Mentors (
-                id int PRIMARY KEY AUTO_INCREMENT,
-                email varchar(255) NOT NULL UNIQUE,
-                discordTag varchar(39) NOT NULL UNIQUE,
-                discordID int
-            );`, err => err ? reject(err) : resolve()
-        );
-    });
+export function initDatabase(db: Database): void {
+    db.run(`
+        CREATE TABLE Teams (
+            number INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );`
+    ).run(`
+        CREATE TABLE Participants (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL UNIQUE,
+            discordTag TEXT NOT NULL UNIQUE,
+            discordID INTEGER,
+            team INTEGER REFERENCES Teams(number)
+        );`
+    ).run(`
+        CREATE TABLE Judges (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL UNIQUE,
+            discordTag TEXT NOT NULL UNIQUE,
+            discordID INTEGER
+        );`
+    ).run(`
+        CREATE TABLE Mentors (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL UNIQUE,
+            discordTag TEXT NOT NULL UNIQUE,
+            discordID INTEGER
+        );`
+    );
 }
